404d666dcefe62de200cd6377748948bc9fda317
Nima Heidari <nima.heidari.n@gmail.com>
2025-10-29 12:43:36 +0330

    feat: add Student model and migration and (config Authenticatable,HasApiTokens)

---
+++ app/Models/Student.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
+
+class Student extends Authenticatable
+{
+    use HasFactory,HasApiTokens;
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'mobile',
+        'gender',
+        'field_study',
+        'email',
+        'password',
+        'inter',
+    ];
+
+}
