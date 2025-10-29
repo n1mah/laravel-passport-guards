bd62116e1d38f377f43b52364f2f8cae51861903
Nima Heidari <nima.heidari.n@gmail.com>
2025-10-29 12:46:24 +0330

    refactor: remove redundant Model import from Admin, Teacher, and Student models

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -1,9 +1,8 @@
-q<?php
+<?php
 
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
 
