429b78b022a9e2d9792de6cd5bb0c84a55925f5a
Nima Heidari <nima.heidari.n@gmail.com>
2025-10-29 12:43:01 +0330

    feat: add Teacher model and migration and (config Authenticatable,HasApiTokens)

---
+++ app/Models/Teacher.php
@@ -0,0 +1,26 @@
+q<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
+
+class Teacher extends Authenticatable
+{
+    use HasFactory,HasApiTokens;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'mobile',
+        'gender',
+        'email',
+        'password',
+        'university',
+        'field_study',
+        'qualification',
+        'experience',
+    ];
+}
